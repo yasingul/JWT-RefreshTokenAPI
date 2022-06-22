a17592a2b188c0626d1f8c9819149c3b5e060eab
Yasin GÜL <89739419+yasingul@users.noreply.github.com>
2022-06-22 19:58:05 +0300

    Add files via upload

---
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SampleDataAccess
+    {
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> output = new();
+
+            output.Add(new() { FirstName = "Yasin", LastName = "Gül" });
+            output.Add(new() { FirstName = "Ali", LastName = "Geç" });
+            output.Add(new() { FirstName = "Fatma", LastName = "Kaşıkçı" });
+            output.Add(new() { FirstName = "Ece", LastName = "Gül" });
+
+            Thread.Sleep(millisecondsTimeout: 3000);
+
+            return output;
+        }
+    }
+}
